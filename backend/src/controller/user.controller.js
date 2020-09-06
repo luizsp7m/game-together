4fc375dde408f0d563ec210d9b3e8109a9785954
Luiz Antonio <luizoliveira2808@gmail.com>
2020-09-06 15:26:54 -0300

    JWT e autenticação

--- backend/src/controller/user.controller.js
+++ backend/src/controller/user.controller.js
@@ -1,4 +1,6 @@
 const UserModel = require('../model/user.model');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken'); 
 
 class UserController {
     async create(req, res) {
@@ -8,6 +10,31 @@ class UserController {
         }).catch(error => {
             return res.status(500).json(error);
         });
+    }
+
+    async authenticate(req, res) {
+        const { email, password } = req.body;
+
+        const user = await UserModel.findOne({ email });
+
+        if (!user) {
+            return res.status(400).json({ error: 'Usuário não cadastrado' });
+        } else {
+            bcrypt.compare(password, user.password, (err, data) => {
+                if (err) throw err
+                if (data) {
+                    const decodedToken = { _id: user._id, email: user.email };
+                    const token = jwt.sign(decodedToken, 'Secret', { expiresIn: '1d' });
+                    return res.status(200).json({
+                        user: user,
+                        token: token
+                    })
+                } else {
+                    return res.status(401).json({ msg: "Invalid credencial" })
+                }
+
+            })
+        }
     }
 
     async update(req, res) {
@@ -27,10 +54,18 @@ class UserController {
     }
 
     async show(req, res) {
-        await UserModel.findById( req.params.id ).then(response => {
+        await UserModel.findById(req.params.id).then(response => {
             return res.status(200).json(response);
         }).catch(error => {
-            return res.status(404).json({error: "Usuário não encontrado"});
+            return res.status(404).json({ error: "Usuário não encontrado" });
+        });
+    }
+
+    async delete(req, res) {
+        await UserModel.deleteOne({ '_id': req.params.id }).then(response => {
+            return res.status(200).json(response);
+        }).catch(error => {
+            return res.status(500).json(error);
         });
     }
 }
--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const UserController = require('../controller/user.controller');
-const UserValidation = require('../Middleware/user.middleware');
+const UserValidation = require('../middleware/user.middleware');
 
 
 router.post('/', UserValidation, UserController.create);
+router.post('/login', UserController.authenticate);
 router.put('/:id', UserController.update);
 router.get('/filter/all', UserController.all);
 router.get('/filter/:id', UserController.show);
+router.delete('/:id', UserController.delete);
 
 module.exports = router;
